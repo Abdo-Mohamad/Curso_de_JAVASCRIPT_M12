620756ffea7e2337ba2ff2f50c702d67e6360236
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-09-23 15:25:00 +0200

    Como se declaran las variables dentro de una funcion y fuera

---
+++ 03_Declaraciones y asignaciones/script.js
@@ -0,0 +1,25 @@
+//  tipos de declaracion  de variables
+var nombreDelVariable = "Pedro";
+let edadDelVariable = 25;
+const nombreDeVariable = "Disponible";
+
+nombreDelVariable = "nuevo nomber";
+// resgnar nueva  valor a una variable declarada con var
+
+let x, y, z;
+x = 10;
+y = 20;
+z = x + y;
+console.log(z);
+// declaracion de variables con let y const
+
+const datoQueVienDeLaClinte = "datos";
+//  datoQueVienDeLaClinte = "nuevo dato";
+
+function nomberDeFuncion() {
+  const nombreVariable = "Varible dentro de Funcion";
+};
+// Como  se declaran las variables dentro de una funcion
+
+
+

048604bc20feebb72de1d1c593b390794211f9f0
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-09-23 15:53:18 +0200

    los tipos de nombres de la variables

---
+++ 06_Sintaxis de las variables/script.js
@@ -0,0 +1,21 @@
+// los tipos de nombres de la variables
+
+const nombreDeLaVariable = "nombre"; // Nombres de la variable
+
+const _varible = "nombre";
+
+const $varible = "nombre";
+
+const Varible = "nombre";
+
+// consta -variable = "nombre"; no se pude
+//  consta_variable = "nombre"; no se pude
+// constaVariable = "nombre"; no se pude
+//  consta-variable = "nombre"; no se pude
+//  consta_variable = "nombre"; no se pude
+// const 8 = "nomber"; no se pude
+
+let nombreapellido = "pedro";
+let nombreApellido = "Dito";
+// los variable no son iguales 
+

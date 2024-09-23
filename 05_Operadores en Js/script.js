cfad8bb81496135d800fd895040d97c4d7359e7d
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-09-23 15:45:34 +0200

    Los operadores de asignación

---
+++ 05_Operadores en Js/script.js
@@ -0,0 +1,39 @@
+// Operadores de asignación
+
+const suma = 1 + 3; // es operador arimético de la suma
+
+const resta = 5 - 2; // es operador arimético de la resta
+
+const multiplicacion = 1 + 3; // es operador arimético de la multiplicacion
+
+const division = 10 / 2; // es operador arimético de la division
+
+const exponente = 2 ** 3; // es operador arimético de la potencia
+
+const modulo = 10 % 3; // es operador arimético de el modulo
+
+const igual = 5 == 5; // es operador de igualdad
+
+const diferente = 5 != 5; // es operador de desigualdad
+
+const mayor = 5 > 3; // es operador de mayor que
+
+const menor = 5 < 3; // es operador de menor que
+
+const mayorIgual = 5 >= 5; // es operador de mayor o igual
+
+const menorIgual = 5 <= 5; // es operador de menor o igual
+
+const and = true && true; // es operador de and
+
+const or = true || false; // es operador de or
+
+let x = 5;
+let y  = 10;
+x++;// incremento de 1
+y--; // decremento 1
+
+console.log(x); // imprime 6
+console.log(y); // imprime 9
+
+

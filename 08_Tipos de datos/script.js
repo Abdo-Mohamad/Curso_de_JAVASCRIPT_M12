05e4161a4b0546e22459512a1604c0bc4adbfcbd
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-09-27 15:49:22 +0200

    Tipos de datos en JS

---
+++ 08_Tipos de datos/script.js
@@ -0,0 +1,46 @@
+// Tipos de datos
+
+let a = 5; // valor entero
+let b = 3.5; // valor decimal o Flornte
+
+// String
+let c = "Cadena de texto"; //  valor texto o cadena o String
+
+// Boleano
+
+let d = true; //  valor booleano o logico verdadero
+let e = false; // valor booleano o logico falso
+
+// Undefindo
+let f; // No  se puede declarar una variable sin asignarle un valor indefinido
+
+// Null
+
+let g = null; // valor nulo o vacio
+
+// objeto
+let objeto = {
+  // valor objeto
+  clave: "valor",
+};
+
+// Array
+let array = [1, 2, 3, 4, 5]; // valor Arreglos
+//  array de valores o lista de valores o conjunto de valores o conjunto de elementos o conjunto de
+let array2 = ["hola", "adios", "chau"]; // valor array de texto o lista
+
+// Date
+let fecha = new Date(); // valor fecha o fecha y hora o fecha y hora actual o fecha
+//  y hora actual o fecha y hora actual o fecha y hora actual o fecha y hora actual
+
+console.log(fecha);
+
+let num1 = 5;
+let num2 = "3";
+//let num3 = num1 +num2;
+//console.log(num3); //  53 Porque  js no hace la suma sino la concatenacion de los valores
+let num3 = num1 + parseInt(num2);
+
+console.log(num3);//  8 porque  js convierte el string a entero y hace la suma
+
+

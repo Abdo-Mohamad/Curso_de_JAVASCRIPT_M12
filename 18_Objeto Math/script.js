bbd6b85d4d34077195db19459847b8b7f1b5a62f
Abdo-Mohamad <abdomohamadwalid@gmail.com>
2024-10-11 15:23:46 +0200

    aprender utilizar math object

---
+++ 18_Objeto Math/script.js
@@ -0,0 +1,17 @@
+// MATH OBJECT
+console.log(Math.PI); //  3.141592653589793
+console.log(Math.E); //   2.718281828459045
+Math.SQRT2 // develve la  raíz cuadrada de 2
+Math.SQRT1_2 // devuelve la raíz cuadrada de 1/2
+Math.LN2 //  devuelve el logaritmo natural de 2
+Math.LOG10 //  devuelve el logaritmo en base 10 de un número
+Math.LOG2E // devuelve  el logaritmo en base 2 de e
+Math.LOG10E // devuelve  el logaritmo en base 10 de e
+
+
+
+console.log(Math.round(2.2));  // 2 hacia arriba 
+console.log(Math.trunc(2.2)); // hacia abajo
+console.log(Math.random()  * 100); // entre 0 y 100
+
+
